0c53316d64732c177e9d5f8a199394bb85e45a44
timstaub2 <timstaub@hotmail.com>
2019-08-04 15:16:01 -0400

    Part 6

---
+++ Nullable_Types/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Nullable_Types
+{
+    class Program
+    {
+        static void Main()
+        {
+            //bool? AreYouMajor = true;
+            //if (AreYouMajor == true)
+            //{
+            //    Console.WriteLine("User is Major");
+            //}
+            //else if (AreYouMajor == false)
+            //{
+            //    Console.WriteLine("User is not Major");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("User did not answer the question");
+            //}
+            int? TicketsOnSale = 100;
+            int AvailableTickests =  TicketsOnSale ?? 0;
+            //if (TicketsOnSale == null)
+            //{
+            //    AvailableTickests = 0;
+            //}
+            //else
+            //{
+            //    AvailableTickests = (int)TicketsOnSale;
+            //}
+
+            Console.WriteLine("Available Tickets for Sale = {0}", AvailableTickests);
+        }
+    }
+}
