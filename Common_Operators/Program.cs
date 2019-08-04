b97615d67332b0022dacab9520290e4ad7903396
timstaub2 <timstaub@hotmail.com>
2019-08-04 14:15:01 -0400

    Update to Common Operators

--- Common_Operators/Program.cs
+++ Common_Operators/Program.cs
@@ -6,12 +6,22 @@ namespace Common_Operators
     {
         static void Main()
         {
-            int Numorator = 10;
-            int Denominator = 2;
+            //int Numorator = 10;
+            //int Denominator = 2;
+            //int Result = Numorator * Denominator;
+            //Console.WriteLine("Result = {0}", Result);
 
-            int Result = Numorator / Denominator;
+            //int Number = 10;
+            //int AnotherNumber = 20;
+            //if (Number == 10 && AnotherNumber ==20)
+            //{
+            //    Console.WriteLine("Hello Maggie");
+            //}
 
-            Console.WriteLine("Result = {0}", Result);
+            int Number = 100;
+            bool IsNumber10 = Number == 10 ? true : false;
+
+            Console.WriteLine("Number == 10 is {0}", IsNumber10);
         }
     }
 }
