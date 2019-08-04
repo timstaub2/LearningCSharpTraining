e9994546b6b8f26425a69cff502d6064cb01afd7
timstaub2 <timstaub@hotmail.com>
2019-08-04 13:33:51 -0400

    Common Operators

---
+++ Common_Operators/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common_Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Numirator = 10;
+            int Denominator = 2;
+            int Result = Numirator / Denominator;
+            Console.WriteLine("Result = {0}", Result);
+        }
+    }
+}
