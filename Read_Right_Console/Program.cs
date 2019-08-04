3591bc767993906c2bd5fcfafe0e7192e03d73bf
timstaub2 <timstaub@hotmail.com>
2019-08-04 11:56:22 -0400

    Read_Right_Console_8419

---
+++ Read_Right_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Please enter your First name");
+        String FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your Last name");
+        String LastName = Console.ReadLine();
+
+        //Console.WriteLine("Hello " + UserName);
+
+        Console.WriteLine("Hello {0}, {1}", FirstName, LastName);
+        }
+    }
