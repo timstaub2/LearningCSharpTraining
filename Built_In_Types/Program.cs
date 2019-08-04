7e249409905dfd74a780242709acf9c47a8ad9e0
timstaub2 <timstaub@hotmail.com>
2019-08-04 12:49:14 -0400

    Built_in_Types, Escape sequences, Berbatim literal

---
+++ Built_In_Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Built_In_Types
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string Name = "\"Timothy\"";
+            //Console.WriteLine(Name);
+
+            //Escape Sequences 
+            //string Name = "One\nTwo\nThree";
+
+            //verbatim literal
+            //string Name = "C:\\Users\\timst\\Source\\Repos\\timstaub2\\LearningCSharpTraining";
+            string Name = @"C:\Users\timst\Source\Repos\timstaub2\LearningCSharpTraining";
+            Console.WriteLine(Name);
+        }
+    }
+}
