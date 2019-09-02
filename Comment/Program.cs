dd946e32690500c5c5b27c14a029b7495e8814f0
timstaub2 <timstaub@hotmail.com>
2019-09-02 14:14:25 -0400

    How to comment out code or make comments in C#

---
+++ Comment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Comment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//there are three different comments 
+//single line ctr+k, ctr+c, ctr+u
+/*mutiple lines
+ * 
+  that should not been seen*/
+  /// <summary>
+  /// 
+  /// </summary>
+public class SampleClass  
