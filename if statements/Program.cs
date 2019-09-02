1c478e170e7069844b6b925eeb4490c19659320c
timstaub2 <timstaub@hotmail.com>
2019-09-02 19:10:17 -0400

    If Statement example with Else, also or

---
+++ if statements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("What state are you from?");
+        
+        var getStateCode = (Console.ReadLine());
+
+
+        if (getStateCode == "MI" | getStateCode == "AL")
+
+            Console.WriteLine("We are the Champions!");
+        else
+            Console.WriteLine("Just a bunch of loosers!");
+
+        }
+    }
+
