95389ec0a6f4eea01813c704d7e54680e93a314a
timstaub2 <timstaub@hotmail.com>
2019-09-02 13:35:18 -0400

    New Array's

---
+++ Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] NewNumber = new int[10];
+        NewNumber[0] = 0;
+        NewNumber[1] = 2;
+        NewNumber[2] = 4;
+        NewNumber[3] = 6;
+        NewNumber[4] = 8;
+        NewNumber[5] = 10;
+        NewNumber[6] = 12;
+        NewNumber[7] = 14;
+        NewNumber[8] = 16;
+        NewNumber[9] = 18;
+        Console.WriteLine(NewNumber[6]);
+    }
+}
+
