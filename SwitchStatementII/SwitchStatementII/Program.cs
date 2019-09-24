bc96195ad3a95451266d78b187ea0afa3b808323
timstaub2 <timstaub@hotmail.com>
2019-09-24 09:47:27 -0400

    Switch statement II

---
+++ SwitchStatementII/SwitchStatementII/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int TotalCoffeeCost = 0;
+
+        Start:
+        Console.WriteLine("Please select your coffee size: 1 - Small, 2 - Medium, 3 - Large");
+        int UserChoice = int.Parse(Console.ReadLine());
+
+        switch (UserChoice)
+        {
+            case 1:
+                TotalCoffeeCost += 1;
+                break;
+            case 2:
+                TotalCoffeeCost += 2;
+                break;
+            case 3:
+                TotalCoffeeCost += 3;
+                break;
+            default:
+                Console.WriteLine("Your choice {0} is invalid", UserChoice);
+                goto Start;
+        }
+        Decide:
+        Console.WriteLine("Do you want to buy another coffee - Yes or No?");
+        string UserDecision = Console.ReadLine();
+
+        switch(UserDecision.ToUpper())
+        {
+            case "YES":
+                goto Start;
+            case "NO":
+                break;
+            default:
+                Console.WriteLine("Your choice is not valid", UserDecision);
+                goto Decide;
+        }
+        Console.WriteLine("Thank you for shopping with us");
+        Console.WriteLine("Bill Amount = ${0}.00", TotalCoffeeCost);
+    }
+}
+
