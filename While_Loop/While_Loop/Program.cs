f96ababb847214013312704f454c16d46fb24689
timstaub2 <timstaub@hotmail.com>
2019-09-24 10:33:43 -0400

    While loop

---
+++ While_Loop/While_Loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your target.");
+
+        int UserTarget = int.Parse(Console.ReadLine());
+
+        int Start = 0;
+
+        while (Start <= UserTarget)
+        {
+            Console.Write(Start + " ");
+            Start = Start + 2;
+        }
+
+    }
+}
