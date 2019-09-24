db1cfeee04cd132fa4dfb519804f35b64bca0e36
timstaub2 <timstaub@hotmail.com>
2019-09-24 11:08:08 -0400

    Do_While loop

---
+++ Do_While_Loop/Do_While_Loop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        string UserChoice = string.Empty;
+        do
+        {
+            Console.WriteLine("Please enter your target.");
+            int UserTarget = int.Parse(Console.ReadLine());
+
+            int Start = 0;
+
+            while (Start <= UserTarget)
+            {
+                Console.Write(Start + " ");
+                Start = Start + 2;
+            }
+            
+            do
+            {
+                Console.WriteLine("Do you want to continue - Yes or No?");
+
+                UserChoice = Console.ReadLine().ToUpper();
+                if (UserChoice != "YES" && UserChoice != "NO")
+                {
+                    Console.WriteLine("Invalid Choice, Please say yes or No");
+                }
+            } while (UserChoice != "YES" && UserChoice != "NO");
+        } while(UserChoice == "YES");
+        }
+    }
+
