7cf5dbeacd433800545dc281768967314dd5665c
timstaub2 <timstaub@hotmail.com>
2019-09-24 08:25:25 -0400

    Switch Statement coding with if statement

---
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main()
+            
+        {
+            Console.WriteLine("please enter a number");
+            int UserNumber = int.Parse(Console.ReadLine());
+
+            switch(UserNumber)
+            {
+                case 10:
+                    Console.WriteLine("Your number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Your number is 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Your number is 30");
+                    break;
+                default:
+                    Console.WriteLine("Your number is not in the group");
+                    break;
+            }
+        }
+    }
+}
